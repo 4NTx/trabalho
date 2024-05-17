bebc0b19c0fe88e3f879ee97cdef3cb57e7a7fbf
4NTx <arturrrick@gmail.com>
2024-05-17 17:38:06 -0300

    fix(global): Corrigido caminho duplicado para views

--- src/main/java/com/artur/Main.java
+++ src/main/java/com/artur/Main.java
@@ -11,8 +11,7 @@ public class Main extends Application {
     @SuppressWarnings("exports")
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource(
-                "/com/artur/view/Main.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/com/artur/view/Main.fxml"));
         primaryStage.setTitle("Imobiliária");
         primaryStage.setScene(new Scene(root, 800, 450));
         primaryStage.centerOnScreen();
